db4b040fb7ff2981bacefea9a0af60493bfa8c73
fedemoretto11 <fedemoretto94@gmail.com>
2023-11-16 08:54:03 -0300

    Primer commit del proyecto

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.red}>Hello There!</Text>
+      <Text style={styles.red}>Soy Fede</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3465cc',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  red: {
+    color: '#f00',
+    fontWeight: 'bold',
+    fontSize: 30,
   },
 });
